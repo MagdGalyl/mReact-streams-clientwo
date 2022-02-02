cda624f17deaf1c9abacda3330c94d20af94e6cf
“Magd <majdgalyl@gmail.com>
2022-02-02 03:20:16 +0200

    gAuth Start Here

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// import GoggleAuth from "./GoggleAuth";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+        {/* <GoggleAuth /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
